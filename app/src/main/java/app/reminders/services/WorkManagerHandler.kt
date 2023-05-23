3103d92a7f27582098713bfe84d58f6a9e83ba0f
Aashutosh Chapagain <chapagainaashutosh96@gmail.com>
2023-05-23 17:41:20 +0545

    [todo] add work manager handler

--- app/src/main/java/app/reminders/services/WorkManagerHandler.kt
+++ app/src/main/java/app/reminders/services/WorkManagerHandler.kt
@@ -1,4 +1,39 @@
 package app.reminders.services
 
-class WorkManagerHandler {
+import android.content.Context
+import androidx.concurrent.futures.CallbackToFutureAdapter
+import androidx.work.ListenableWorker
+import androidx.work.WorkerParameters
+import app.reminders.data.database.AppDao
+import com.google.common.util.concurrent.ListenableFuture
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+interface MyCallback {
+    fun onSuccess()
+    fun onError()
+}
+
+class WorkManagerHandler(val context: Context, private val workerParams: WorkerParameters): ListenableWorker(context, workerParams), KoinComponent {
+    private val appDao: AppDao by inject()
+
+    override fun startWork(): ListenableFuture<Result> {
+        return CallbackToFutureAdapter.getFuture {
+            val callback = object : MyCallback {
+                override fun onSuccess() {
+                    it.set(Result.success())
+                }
+
+                override fun onError() {
+                    it.set(Result.failure())
+                }
+            }
+            initReminders(callback)
+            return@getFuture callback
+        }
+    }
+
+    private fun initReminders(callback: MyCallback) {
+        TODO("Not yet implemented")
+    }
 }
